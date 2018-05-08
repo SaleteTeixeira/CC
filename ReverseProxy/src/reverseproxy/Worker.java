f6208a86b0efc70b8f03ad21e83658b6fba0f7ec
SaleteTeixeira <salete.diasteixeira@hotmail.com>
2018-05-08 12:10:25 +0100

    reserseproxy feito

--- ReverseProxy/src/reverseproxy/Worker.java
+++ ReverseProxy/src/reverseproxy/Worker.java
@@ -1,8 +1,15 @@
 package reverseproxy;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Thread que recebe mensagens de um Cliente, atribuindo-lhe um Agente da Tabela e mantem a comunicação do cliente para esse Agente
@@ -21,25 +28,48 @@ public class Worker implements Runnable{
     }
     
     public void run(){
-        Info agenteEscolhido;
-        InetAddress IPagente;
-        int portaAgente;
-        
-        this.lockTabela.lock();
+        try {
+            Info agenteEscolhido;
+            InetAddress IPagente;
+            Socket sc;
+            BufferedReader in;
+            BufferedWriter out;
+            String str = null;
             
-        agenteEscolhido = this.tabela.algoritmoSelecao();
+            this.lockTabela.lock();
             
-        if(agenteEscolhido != null){
+            agenteEscolhido = this.tabela.algoritmoSelecao();
+            
+            if(agenteEscolhido == null){
+                this.lockTabela.unlock();
+                Thread.currentThread().interrupt();
+            }
+            
             IPagente = agenteEscolhido.getIpS();
-            portaAgente = agenteEscolhido.getPortaS();
-        }
             
-        this.lockTabela.unlock();
-        
-        //abre uma conexão TCP para o servidor escolhido
-        
-        while(true){
-            //recebe pedidos cliente
+            this.lockTabela.unlock();
+            
+            sc = new Socket(IPagente,80);
+            
+            Thread ler = new Thread(new LerHttp(this.socket,sc));
+            ler.start();
+            
+            in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+            out = new BufferedWriter(new OutputStreamWriter(sc.getOutputStream()));
+            
+            while((str = in.readLine())!= null){
+                out.write(str);
+                out.flush();
+                System.out.println("ENVIEI PARA O AGENTE: "+str);
+            }
+            
+            sc.shutdownOutput();
+            this.socket.shutdownInput();
+            sc.close();
+            this.socket.close();
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Worker.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
